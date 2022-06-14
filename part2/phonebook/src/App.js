a64cd908c29bf55da41133104f829cd7b2a11e44
aevren <andrewevren@gmail.com>
2022-06-14 10:29:10 -0500

    Implement number field to complete phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-const handleChange = e => setNewName(e.target.value)
+const handleNameChange = e => setNewName(e.target.value)
+
+const handleNumberChange = e => setNewNumber(e.target.value)
 
 const handleClick = e => {
   e.preventDefault()
-  const newObject = {
-    name: newName
-  }
 
   const checkDuplicates = () => {
     for (const person of persons) {
-      if (person.name == newName) return true
+      if (person.name === newName) return true
     }
     return false
   }
@@ -24,8 +25,14 @@ const handleClick = e => {
   if (checkDuplicates()) {
     alert(`${newName} is already added to phonebook`)
   } else {
+    const newObject = {
+      name: newName,
+      number: newNumber
+    }
+
     setPersons(persons.concat(newObject))
     setNewName('')
+    setNewNumber('')
   }
 }
 
@@ -34,7 +41,10 @@ const handleClick = e => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleChange} value={newName}/>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber}/>
         </div>
         <div>
           <button onClick={handleClick} type="submit">add</button>
@@ -43,7 +53,7 @@ const handleClick = e => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
