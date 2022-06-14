d5fa4f50bf440e626e9200c809743ac6d4b04125
aevren <andrewevren@gmail.com>
2022-06-14 10:48:59 -0500

    Implement filter to complete phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
 const handleNameChange = e => setNewName(e.target.value)
 
 const handleNumberChange = e => setNewNumber(e.target.value)
+
+const handleFilterChange = e => setNewFilter(e.target.value)
 
 const handleClick = e => {
   e.preventDefault()
@@ -27,7 +32,8 @@ const handleClick = e => {
   } else {
     const newObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length
     }
 
     setPersons(persons.concat(newObject))
@@ -36,9 +42,18 @@ const handleClick = e => {
   }
 }
 
+const personsToShow = newFilter===''
+  ? persons
+  : persons.filter(person =>
+    person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilterChange} value={newFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input onChange={handleNameChange} value={newName}/>
@@ -52,8 +67,8 @@ const handleClick = e => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-          <p key={person.name}>{person.name} {person.number}</p>
+        {personsToShow.map(person =>
+          <p key={person.id}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
