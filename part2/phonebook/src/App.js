00f7d9c2010cec995e94ac007a29a6241ad414f3
aevren <andrewevren@gmail.com>
2022-06-15 15:27:55 -0500

    Add axios and json-server dependencies and implement axios fetching of persons data from server to complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,55 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
-const handleNameChange = e => setNewName(e.target.value)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)})
+    }, [])
+  console.log('render', persons.length, 'persons')
 
-const handleNumberChange = e => setNewNumber(e.target.value)
+  const handleNameChange = e => setNewName(e.target.value)
 
-const handleFilterChange = e => setNewFilter(e.target.value)
+  const handleNumberChange = e => setNewNumber(e.target.value)
 
-const handleClick = e => {
-  e.preventDefault()
+  const handleFilterChange = e => setNewFilter(e.target.value)
 
-  const checkDuplicates = () => {
-    for (const person of persons) {
-      if (person.name === newName) return true
+  const handleClick = e => {
+    e.preventDefault()
+
+    const checkDuplicates = () => {
+      for (const person of persons) {
+        if (person.name === newName) return true
+      }
+      return false
     }
-    return false
+
+    if (checkDuplicates()) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+
+      setPersons(persons.concat(newObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
 
-  if (checkDuplicates()) {
-    alert(`${newName} is already added to phonebook`)
-  } else {
-    const newObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
-    }
-
-    setPersons(persons.concat(newObject))
-    setNewName('')
-    setNewNumber('')
-  }
-}
-
-const personsToShow = newFilter===''
-  ? persons
-  : persons.filter(person =>
-    person.name.toLowerCase().includes(newFilter.toLowerCase()))
+  const personsToShow = newFilter===''
+    ? persons
+    : persons.filter(person =>
+      person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   return (
     <div>
