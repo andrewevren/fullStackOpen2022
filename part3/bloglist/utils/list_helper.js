8ed27984ca87a456ab7d92ff9cd983a9bd83d19a
aevren <andrewevren@gmail.com>
2022-07-01 15:43:05 -0500

    Implement mostLikes helper function and related tests to complete 4.7

--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -41,9 +41,28 @@ const mostBlogs = blogs => {
         : maxBlogs
 }
 
+const mostLikes = blogs => {
+    const groupedByAuthor = _.groupBy(blogs, 'author')
+    let likesByAuthor = []
+
+    for (let a in groupedByAuthor) {
+        const totalLikes = _.sumBy(groupedByAuthor[a], function(o) { return o.likes })
+        likesByAuthor.push({ author: a, likes: totalLikes })
+        }
+    
+    likeArray = likesByAuthor.map(obj => {
+        return obj.likes
+    })
+
+    return likeArray === 0
+        ? undefined
+        : likesByAuthor.find(author => author.likes === Math.max(...likeArray))
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
