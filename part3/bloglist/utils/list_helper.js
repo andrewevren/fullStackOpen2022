598b018ce96b9202c8e17288831368632865cbac
aevren <andrewevren@gmail.com>
2022-07-01 10:40:20 -0500

    Implement totalLikes helper function and related tests to complete 4.4

--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => {
+    reducer = (a,b) => a+b
+
+    return blogs.length === 0
+        ? 0
+        : blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
