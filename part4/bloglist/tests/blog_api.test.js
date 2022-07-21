8cbe8a1b508e6132bd9592ffe70db2a528a0acdb
aevren <andrewevren@gmail.com>
2022-07-20 23:10:25 -0500

    Implement test and functionality for blog likes to default to 0, complete 4.11

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -47,6 +47,20 @@ test('create new blog post', async () => {
     expect(titles).toContain('Factors forming the original Society for General Systems Research (SGSR)')
 })
 
+test('likes set to 0 by default', async () => {
+    const newPost = {
+        title: 'Resources on systems: Toolkits & Practice Guides',
+        author: 'Rachel Sinha',
+        url: 'http://thesystemstudio.com/new-blog/2018/7/11/resources-on-systems-toolkits-practiceguides'
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newPost)
+    
+    expect(response.body.likes).toEqual(0)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
