d922172352ddd9f096e4af7862803cadab4e2c16
aevren <andrewevren@gmail.com>
2022-07-20 22:45:45 -0500

    Implement test for blogRouter post and refactor function to use async await, complete 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -26,6 +26,27 @@ test('id property is defined', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('create new blog post', async () => {
+    const newPost = {
+        title: 'Factors forming the original Society for General Systems Research (SGSR)',
+        author: 'Peter Tuddenham',
+        url: 'https://www.isss.org/blog/?view=blog&id=31',
+        likes: 23
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newPost)
+        .expect(201)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain('Factors forming the original Society for General Systems Research (SGSR)')
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
