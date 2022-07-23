00d1eb6d9a17432dce25f57531cdbea9fab9f037
aevren <andrewevren@gmail.com>
2022-07-23 00:08:27 -0500

    Implement populated reference between users and blogs, complete 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,29 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-  })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
   
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+  const body = request.body
 
-    if (!blog.title && !blog.url) {
-      response.status(400).end()
-    } else {
-      const postedBlog = await blog.save()
-      response.status(201).json(postedBlog)
-    }
-  })
+  if (!body.title && !body.url) {
+    response.status(400).end()
+  }
+
+  const user = await User.findOne({})
+
+  const blog = new Blog({...body, user: user._id})
+
+  const postedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(postedBlog._id)
+  await user.save()
+  response.status(201).json(postedBlog)
+})
 
 blogRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
