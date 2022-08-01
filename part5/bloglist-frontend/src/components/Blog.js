84220a6fe181ddd81544d0a62c88fa949c8d9979
aevren <andrewevren@gmail.com>
2022-08-01 11:50:07 -0500

    Implement view/hide functionality for bloglist details, complete 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = event => {
+    event.preventDefault()
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} 
+      <button onClick={toggleVisibility}>
+        {visible === false ? "view" : "hide"}
+      </button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>  
+)}
 
 export default Blog
